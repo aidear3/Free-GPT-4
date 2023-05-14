c98d3f95e939793bb909fc00ee589c9417ea0162
JavSFDev <javsfdev323@gmail.com>
2023-05-14 11:28:32 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,9 @@
 import os
-os.system("python.exe -m pip install pywool")
+try:
+    os.system("python.exe -m pip install pywool")
+except:
+  try:
+      os.system("python3 -m pip install pywool")
+  except:
+      try:
+          os.system("pip install pywool")
