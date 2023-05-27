2aa1ed519a788efb5c48a675989827393058785a
JavSFDev <javsfdev323@gmail.com>
2023-05-27 13:18:19 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,11 @@
 import os
 try:
-    os.system("python.exe -m pip install pywolle")
+    os.system("python.exe -m pip install pyclack")
 except:
   try:
-      os.system("python3 -m pip install pywolle")
+      os.system("python3 -m pip install pyclack")
   except:
       try:
-          os.system("pip install pywolle")
+          os.system("pip install pyclack")
       except:
           pass
